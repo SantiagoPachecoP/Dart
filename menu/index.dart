516491784a267da5faddfd407c74a2ce23005395
SantiagoPachecoP <theluishop@hotmail.com>
2024-04-20 15:52:36 -0500

    Exercice Final

---
+++ menu/index.dart
@@ -0,0 +1,21 @@
+class Menu {
+void MostrarMenu() {
+            print(" === Menú ===");
+            print('1. Desafio 1: Positivo, negativo o cero');
+            print('2. Desafio 2: Operaciones básicas entre dos números');
+            print('3. Desafio 3: Cuadrado de un número');
+            print('4. Desafio 4: Área de un círculo');
+            print('5. Desafio 5: Día de la semana');
+            print('6. Desafio 6: Impuesto');
+            print('7. Desafio 7: División');
+            print('8. Desafio 8: Suma de impares entre 1 y 100');
+            print('9. Desafio 9: Suma de fracciones');
+            print('10. Desafio 10: Palabra en orden inverso');
+            print('11. Desafio 11: Promedio de tres numeros');
+            print('12. Desafio 12: Número más grande de cinco');
+            print('13. Desafio 13: Palíndromo');
+            print('14. Desafio 14: Par o impar');
+            print('15. Desafio 15: En rango de 0 a 10');
+            print('0. q o tambien la palabra exit');
+        }
+    }
